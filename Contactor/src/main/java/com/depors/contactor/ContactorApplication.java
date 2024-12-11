9af7522152c4a22b40610da9400edf02002865e8
DeporS <depores1337@gmail.com>
2024-12-11 21:44:54 +0100

    create packages

--- Contactor/src/main/java/com/depors/contactor/ContactorApplication.java
+++ Contactor/src/main/java/com/depors/contactor/ContactorApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ContactorApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ContactorApplication.class, args);
     }
 
